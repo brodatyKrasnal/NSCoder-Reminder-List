906ee635dcb769d7048c5f80c489e4292752881d
Tymon Goleba Frygies <tymon.gf@hotmail.com>
2020-09-01 21:23:41 +0200

    Initial Commit

---
+++ NSCoder Reminder List/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NSCoder Reminder List : ViewController.swift by tymek on 01/09/2020 21:23.
+//  Copyright © 2020  Tymon Golęba Frygies. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
